ec64fcca929c53f866e7077612db4ece5313c82c
Estudiantes <Estudiantes@Labing50415.udistrital.edu.co>
2018-02-09 11:50:31 -0500

    clase principal

---
+++ miCalculadora/src/micalculadora/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        //crar un objeto de la clase calculadora
+        Calculadora miCalculadora; //definicion
+        miCalculadora = new Calculadora(); // construir el objeto
+    }
+
+}
