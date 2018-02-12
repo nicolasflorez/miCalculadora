dcb3f7742848c8436cbee0761500b4e4eae45310
Estudiantes <Estudiantes@Labing70216>
2018-02-12 18:04:51 -0500

    herancia con trigonometrica

---
+++ Calculadora/src/micalculadora/Trigonometrica.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package micalculadora;
+    
+public class Trigonometrica extends Calculadora
+{
+/**
+ *
+ * @author Estudiantes
+ */
+
+    double coseno;
+    return cos(x);
+}
